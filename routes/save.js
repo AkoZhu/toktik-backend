939a0d1203ff79f965a0910686097c98ad92c3b0
AkoZhu <zxuanbiao@outlook.com>
2023-09-06 19:30:50 -0400

    ✨🐛 routes(save): Fix save.js file formatting and add support for local storage
    The `save.js` file in the `routes` directory had some formatting issues and
    lacked support for local storage. This commit fixes the formatting issues and
    adds support for local storage when the required environment variables for R2
    storage are not provided. Now, the file can be saved locally if the R2 storage
    is not configured.
    
    - Fix formatting issues in `save.js` file
    - Add support for local storage when R2 storage is not configured
    - Update file URLs and paths accordingly

--- routes/save.js
+++ routes/save.js
@@ -3,26 +3,29 @@ const multer = require('multer');
 const multerS3 = require('multer-s3')
 const path = require("path");
 const {SaveFileError} = require("../errors/saveError");
-const {S3Client} = require('@aws-sdk/client-s3')
+const {S3Client} = require('@aws-sdk/client-s3');
+const fs = require('fs');
 
 require('dotenv').config();
 
 const router = express.Router();
 
-const r2 = new S3Client(
-    {
-        region: 'auto',
-        endpoint: `https://${process.env.R2_ACCOUNT_ID}.r2.cloudflarestorage.com`,
-        credentials: {
-            accessKeyId: process.env.R2_ACCOUNT_KEY,
-            secretAccessKey: process.env.R2_ACCOUNT_SECRET,
-        },
-        signatureVersion: 'v4',
-    }
-)
+let storage;
+const LOCAL_PUBLIC_URL = "http://localhost:8080";
 
-const upload = multer({
-    storage: multerS3({
+if (process.env.R2_ACCOUNT_ID && process.env.R2_ACCOUNT_KEY && process.env.R2_ACCOUNT_SECRET && process.env.R2_BUCKET_NAME && process.env.R2_PUBLIC_URL) {
+    const r2 = new S3Client(
+        {
+            region: 'auto',
+            endpoint: `https://${process.env.R2_ACCOUNT_ID}.r2.cloudflarestorage.com`,
+            credentials: {
+                accessKeyId: process.env.R2_ACCOUNT_KEY,
+                secretAccessKey: process.env.R2_ACCOUNT_SECRET,
+            },
+            signatureVersion: 'v4',
+        }
+    )
+    storage = multerS3({
         s3: r2,
         bucket: process.env.R2_BUCKET_NAME,
         metadata: function (req, file, cb) {
@@ -33,7 +36,25 @@ const upload = multer({
             cb(null, Date.now().toString() + '.' + file.originalname.split('.').pop())
         },
         contentType: multerS3.AUTO_CONTENT_TYPE,
-    }),
+    })
+} else {
+    const uploadDirectory = 'uploads/';
+
+    if (!fs.existsSync(uploadDirectory)) {
+        fs.mkdirSync(uploadDirectory);
+    }
+    storage = multer.diskStorage({
+        destination: function (req, file, cb) {
+            cb(null, 'uploads/') // 'uploads/' is the directory where files will be stored locally
+        },
+        filename: function (req, file, cb) {
+            cb(null, Date.now() + path.extname(file.originalname)); // set the file name to current timestamp + file extension
+        }
+    });
+}
+
+const upload = multer({
+    storage: storage,
     limits: {
         fileSize: 1000000000 // 1000000 Bytes = 1 MB
     },
@@ -53,8 +74,10 @@ router.post('/one', upload.single('file'), async (req, res, next) => {
             return;
         }
 
+        const baseUrl = file.key ? process.env.R2_PUBLIC_URL : LOCAL_PUBLIC_URL;
+        const filePath = file.key ? file.key : file.path;
         const resp = {
-            url: process.env.R2_PUBLIC_URL + "/" + file.key,
+            url: baseUrl + "/" + filePath,
             type: file.mimetype.startsWith('image/') ? 0 : 1,
         }
 
@@ -73,8 +96,10 @@ router.post('/multiple', upload.array('file[]'), async (req, res, next) => {
     try {
         let files = [];
         for (let file of req.files) {
+            const baseUrl = file.key ? process.env.R2_PUBLIC_URL : LOCAL_PUBLIC_URL;
+            const filePath = file.key ? file.key : file.path;
             files.push({
-                url: process.env.R2_PUBLIC_URL + "/" + file.key,
+                url: baseUrl + "/" + filePath,
                 type: file.mimetype.startsWith('image/') ? 0 : 1,
             });
         }
